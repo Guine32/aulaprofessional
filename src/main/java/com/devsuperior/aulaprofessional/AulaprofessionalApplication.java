f061a85192f0f4e0e22ef69f3a90487c8fe72693
Wagner Vinicius <wagnervsantos@hotmail.com>
2025-08-27 18:14:06 -0300

    order

---
+++ src/main/java/com/devsuperior/aulaprofessional/AulaprofessionalApplication.java
@@ -0,0 +1,14 @@
+package com.devsuperior.aulaprofessional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaprofessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaprofessionalApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/devsuperior/aulaprofessional/AulaprofessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulaprofessional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaprofessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
