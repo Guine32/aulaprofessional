13ca4310149a812d35c815ea2348e6d8133be248
Wagner Vinicius <wagnervsantos@hotmail.com>
2025-09-01 01:25:48 -0300

    Equals, hashCode

--- src/main/java/com/devsuperior/aulaprofessional/entities/Order.java
+++ src/main/java/com/devsuperior/aulaprofessional/entities/Order.java
@@ -95,8 +95,30 @@ public class Order {
         return items;
     }
 
-    public List<Product> geProducts(){
+    public List<Product> geProducts() {
         return items.stream().map(x -> x.getProduct()).toList();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id != other.id)
+            return false;
+        return true;
     }
 
 }
