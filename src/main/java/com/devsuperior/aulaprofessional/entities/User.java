d170731d18ce77ead6dd627ad7e39508e25807e1
Wagner Vinicius <wagnervsantos@hotmail.com>
2025-09-01 01:08:08 -0300

    Database seeding, correção User birthDate

--- src/main/java/com/devsuperior/aulaprofessional/entities/User.java
+++ src/main/java/com/devsuperior/aulaprofessional/entities/User.java
@@ -23,7 +23,7 @@ public class User {
     @Column(unique = true)
     private String email;
     private String phone;
-    private LocalDate bithDate;
+    private LocalDate birthDate;
     private String password;
 
     @OneToMany(mappedBy = "client")
@@ -33,12 +33,12 @@ public class User {
 
     }
 
-    public User(Long id, String name, String email, String phone, LocalDate bithDate, String password) {
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
-        this.bithDate = bithDate;
+        this.birthDate = birthDate;
         this.password = password;
     }
 
@@ -74,12 +74,12 @@ public class User {
         this.phone = phone;
     }
 
-    public LocalDate getBithDate() {
-        return bithDate;
+    public LocalDate getBirthDate() {
+        return birthDate;
     }
 
-    public void setBithDate(LocalDate bithDate) {
-        this.bithDate = bithDate;
+    public void setBithDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
     }
 
     public String getPassword() {
